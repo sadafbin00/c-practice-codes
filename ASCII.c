6c369c7f968a87718938f769c7d5a3ef0d2673a7
sadafbin00 <86516323+sadafbin00@users.noreply.github.com>
2021-11-05 12:59:44 -0700

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("\nEnter any character =");
+    scanf("%c",&ch);
+    printf("\nASCII Value for this character = %d\n",ch);
+
+    int num;
+    printf("\nEnter any ASCII value =");
+    scanf("%d",&num);
+    printf("\nCharacter of this value = %c\n",num);
+    return 0;
+}
