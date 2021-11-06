c7247c839498260dce427980a0f8f186b3a152a2
sadafbin00 <86516323+sadafbin00@users.noreply.github.com>
2021-11-06 01:44:52 -0700

    Add files via upload

---
+++ Area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int length, width, area;
+printf("Enter a length value: %d\n");
+scanf("%d",&length);
+printf("Enter a width value: %d\n");
+scanf("%d",&width);
+area=length*width;
+printf("Area of this value = %d\n",area);
+scanf("%d","%d",&length,&width);
+return 0;
+}
