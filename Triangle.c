be1c1e8e967cec6fb6d6dce8a71ce21d36df840a
sadafbin00 <86516323+sadafbin00@users.noreply.github.com>
2021-11-06 01:48:24 -0700

    Add files via upload

---
+++ Triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+float height, base, area;
+printf("Put a height value: ");
+scanf("%f",&height);
+printf("Put a base value: ");
+scanf("%f",&base);
+area=(.5*height*base);
+printf("Area of this Triangle: %f",area);
+return 0;
+}
